922a3da62c40da164f45255370e39e7d44612e30
yazan99sh <syria.yazan19@gmail.com>
2022-02-20 21:46:24 +0200

    new widgets

---
+++ store_owner/lib/module_branches/ui/widget/custom_icon_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class CustomIconButton extends StatelessWidget {
+  final IconData icon;
+  final Color backgroundColor;
+  final Color iconColor;
+  final VoidCallback onTap;
+
+  const CustomIconButton(
+      {Key? key,
+      required this.backgroundColor,
+      required this.icon,
+      required this.iconColor,
+      required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      onTap: onTap,
+      child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: backgroundColor,
+              boxShadow: [
+                BoxShadow(
+                    color: Theme.of(context).backgroundColor,
+                    blurRadius: 12,
+                    spreadRadius: 2.5,
+                    offset: Offset(-0.5, 0))
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              icon,
+              color: iconColor,
+            ),
+          )),
+    );
+  }
+}
