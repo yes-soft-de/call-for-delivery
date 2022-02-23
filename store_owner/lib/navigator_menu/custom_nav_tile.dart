0c5a9e258f488d700d0447b731af496815865bb8
yazan99sh <syria.yazan19@gmail.com>
2022-02-23 18:18:53 +0200

    drwer menu

---
+++ store_owner/lib/navigator_menu/custom_nav_tile.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CustomNavTile extends StatelessWidget {
+  final VoidCallback onTap;
+  final String title;
+  final IconData icon;
+  const CustomNavTile(
+      {Key? key, required this.icon, required this.onTap, required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: Scaffold(
+        body: InkWell(
+          borderRadius: BorderRadius.circular(25),
+          splashColor:
+              Theme.of(context).colorScheme.primaryContainer.withOpacity(0.5),
+          highlightColor: Theme.of(context).backgroundColor,
+          splashFactory: InkRipple.splashFactory,
+          onTap: onTap,
+          child: ListTile(
+            leading: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                // boxShadow: [
+                //   BoxShadow(
+                //       color: Theme.of(context).colorScheme.primaryContainer,
+                //       spreadRadius: 1.5,
+                //       blurRadius: 6,
+                //       offset: Offset(-0.2, 0)),
+                // ],
+                color: Theme.of(context).colorScheme.primaryContainer,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  icon,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+            title: Text(title),
+          ),
+        ),
+      ),
+    );
+  }
+}
