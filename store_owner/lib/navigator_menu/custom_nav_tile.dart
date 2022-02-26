7f6173005b24837c8461aec3b278dc745cc347b6
yazan99sh <syria.yazan19@gmail.com>
2022-02-26 15:16:10 +0200

    hide overflowed border

--- store_owner/lib/navigator_menu/custom_nav_tile.dart
+++ store_owner/lib/navigator_menu/custom_nav_tile.dart
@@ -10,38 +10,42 @@ class CustomNavTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 60,
-      child: Scaffold(
-        body: InkWell(
-          borderRadius: BorderRadius.circular(25),
-          splashColor:
-              Theme.of(context).colorScheme.primaryContainer.withOpacity(0.5),
-          highlightColor: Theme.of(context).backgroundColor,
-          splashFactory: InkRipple.splashFactory,
-          onTap: onTap,
-          child: ListTile(
-            leading: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                // boxShadow: [
-                //   BoxShadow(
-                //       color: Theme.of(context).colorScheme.primaryContainer,
-                //       spreadRadius: 1.5,
-                //       blurRadius: 6,
-                //       offset: Offset(-0.2, 0)),
-                // ],
-                color: Theme.of(context).colorScheme.primaryContainer,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(
-                  icon,
-                  color: Theme.of(context).colorScheme.primary
+    return Padding(
+      padding: const EdgeInsets.only(left:8.0,right: 8.0),
+      child: SizedBox(
+        height: 60,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: InkWell(
+            borderRadius: BorderRadius.circular(25),
+            splashColor:
+                Theme.of(context).colorScheme.primaryContainer.withOpacity(0.5),
+            highlightColor: Theme.of(context).backgroundColor,
+            splashFactory: InkRipple.splashFactory,
+            onTap: onTap,
+            child: ListTile(
+              leading: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  // boxShadow: [
+                  //   BoxShadow(
+                  //       color: Theme.of(context).colorScheme.primaryContainer,
+                  //       spreadRadius: 1.5,
+                  //       blurRadius: 6,
+                  //       offset: Offset(-0.2, 0)),
+                  // ],
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    icon,
+                    color: Theme.of(context).colorScheme.primary
+                  ),
                 ),
               ),
+              title: Text(title),
             ),
-            title: Text(title),
           ),
         ),
       ),
