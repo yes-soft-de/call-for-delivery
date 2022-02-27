f854de9617052028b3932292027e2c435e6146ce
ralsalloum <ramisalloum12@hotmail.com>
2022-02-28 01:30:51 +0200

    image constant classes were added

---
+++ backend/src/Constant/Image/ImageAimConstant.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Constant\Image;
+
+class ImageAimConstant
+{
+    const IMAGE_AIM_PROFILE_IMAGE = "profileImage";
+
+    const IMAGE_AIM_MECHANIC_LICENSE_IMAGE = "mechanicLicense";
+
+    const IMAGE_AIM_DRIVE_LICENSE_IMAGE = "driveLicense";
+
+    const IMAGE_AIM_ORDER_IMAGE = "orderImage";
+}
---
+++ backend/src/Constant/Image/ImageEntityTypeConstant.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constant\Image;
+
+final class ImageEntityTypeConstant
+{
+    const ENTITY_TYPE_ORDER = "order";
+
+    const ENTITY_TYPE_STORE_OWNER_PROFILE = "storeOwnerProfile";
+
+    const ENTITY_TYPE_CAPTAIN_PROFILE = "captainProfile";
+}
