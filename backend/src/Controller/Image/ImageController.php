5a3b18fc8faa7559a2089224b24d68a94a596930
ralsalloum <ramisalloum12@hotmail.com>
2022-03-02 21:43:58 +0200

    create image API was removed

--- backend/src/Controller/Image/ImageController.php
+++ backend/src/Controller/Image/ImageController.php
@@ -2,17 +2,11 @@
 
 namespace App\Controller\Image;
 
-use App\AutoMapping;
 use App\Controller\BaseController;
-use App\Request\Image\ImageCreateRequest;
 use App\Service\Image\ImageService;
-use stdClass;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 use OpenApi\Annotations as OA;
 
 /**
@@ -20,61 +14,9 @@ use OpenApi\Annotations as OA;
  */
 class ImageController extends BaseController
 {
-    public function __construct(SerializerInterface $serializer, private AutoMapping $autoMapping, private ValidatorInterface $validator, private ImageService $imageService)
+    public function __construct(SerializerInterface $serializer, private ImageService $imageService)
     {
         parent::__construct($serializer);
-    }
-
-    /**
-     * Create new image. THIS JUST FOR TESTING CREATE IMAGE FUNCTIONS
-     * @Route("create", name="createImage", methods={"POST"})
-     * @param Request $request
-     * @return JsonResponse
-     *
-     * @OA\Tag(name="Image Test")
-     *
-     * @OA\RequestBody(
-     *      description="Create new image",
-     *      @OA\JsonContent(
-     *          @OA\Property(type="string", property="imagePath"),
-     *          @OA\Property(type="integer", property="entityType"),
-     *          @OA\Property(type="integer", property="usedAs"),
-     *          @OA\Property(type="integer", property="itemId")
-     *      )
-     * )
-     *
-     * @OA\Response(
-     *      response=201,
-     *      description="Returns the new image info",
-     *      @OA\JsonContent(
-     *          @OA\Property(type="string", property="status_code"),
-     *          @OA\Property(type="string", property="msg"),
-     *          @OA\Property(type="object", property="Data",
-     *                  @OA\Property(type="integer", property="id"),
-     *                  @OA\Property(type="string", property="imagePath"),
-     *                  @OA\Property(type="integer", property="entityType"),
-     *                  @OA\Property(type="integer", property="usedAs"),
-     *                  @OA\Property(type="integer", property="itemId")
-     *          )
-     *      )
-     * )
-     */
-    public function createImage(Request $request): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
-
-        $request = $this->autoMapping->map(stdClass::class, ImageCreateRequest::class, (object)$data);
-
-        $violations = $this->validator->validate($request);
-        if (\count($violations) > 0) {
-            $violationsString = (string) $violations;
-
-            return new JsonResponse($violationsString, Response::HTTP_OK);
-        }
-
-        $response = $this->imageService->create($request);
-
-        return $this->response($response, self::CREATE);
     }
 
     /**
