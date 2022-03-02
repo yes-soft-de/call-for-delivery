7f573263617604c712321f7a074f4210527ed087
ralsalloum <ramisalloum12@hotmail.com>
2022-03-03 01:24:02 +0200

    store owner branch constant were updated

--- backend/src/Constant/StoreOwnerBranch/StoreOwnerBranch.php
+++ backend/src/Constant/StoreOwnerBranch/StoreOwnerBranch.php
@@ -7,4 +7,8 @@ final class StoreOwnerBranch
     const BRANCH_NOT_FOUND = "not found";
 
     const BRANCHES_NOT_PROVIDED = "no branches were provided";
+
+    const BRANCH_IS_ACTIVE = 1;
+
+    const BRANCH_IS_NOT_ACTIVE = 0;
 }
