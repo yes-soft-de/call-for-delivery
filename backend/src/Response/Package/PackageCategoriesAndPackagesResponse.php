8b814d51fc454292884ee7864bf9cf844579e243
ralsalloum <ramisalloum12@hotmail.com>
2022-03-03 22:01:43 +0200

    type field annotation were added

--- backend/src/Response/Package/PackageCategoriesAndPackagesResponse.php
+++ backend/src/Response/Package/PackageCategoriesAndPackagesResponse.php
@@ -4,11 +4,23 @@ namespace App\Response\Package;
 
 class PackageCategoriesAndPackagesResponse
 {
+    /**
+     * @var int|null
+     */
     public $id;
 
+    /**
+     * @var string|null
+     */
     public $name;
 
+    /**
+     * @var string|null
+     */
     public $description;
-    
+
+    /**
+     * @var array|null
+     */
     public $packages;
 }
