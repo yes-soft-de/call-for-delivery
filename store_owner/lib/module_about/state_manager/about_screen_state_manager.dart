e575924db67c8ebcd9fa1ad0fd3be9e81bb8e600
yazan99sh <syria.yazan19@gmail.com>
2022-03-06 18:23:45 +0200

    add packages to about

--- store_owner/lib/module_about/state_manager/about_screen_state_manager.dart
+++ store_owner/lib/module_about/state_manager/about_screen_state_manager.dart
@@ -1,7 +1,14 @@
+import 'package:c4d/di/di_config.dart';
+import 'package:c4d/generated/l10n.dart';
 import 'package:c4d/module_about/service/about_service/about_service.dart';
+import 'package:c4d/module_about/ui/screen/about_screen/about_screen.dart';
 import 'package:c4d/module_about/ui/states/about/about_state.dart';
-import 'package:c4d/module_about/ui/states/about/about_state_request_appointment.dart';
+import 'package:c4d/module_about/ui/states/about/about_state_page_owner.dart';
 import 'package:c4d/module_localization/service/localization_service/localization_service.dart';
+import 'package:c4d/module_subscription/model/packages.model.dart';
+import 'package:c4d/module_subscription/response/package_categories_response/package.dart';
+import 'package:c4d/module_subscription/service/subscription_service.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:injectable/injectable.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -19,19 +26,23 @@ class AboutScreenStateManager {
     this._localizationService,
     this._aboutService,
   );
-
-  void setLanguage(String lang) {
-    _localizationService.setLanguage(lang);
-  }
-
   void requestBooking() {
     // _stateSubject.add(AboutStateRequestBooking());
   }
-
-  void moveNext() {
-    // _stateSubject.add(AboutStateLoading(this));
-    // _initAccountService.getPackages().then((packages) {
-    //   _stateSubject.add(AboutStatePageOwner(this, packages));
-    // });
+  void getPackages(AboutScreenState screenState) async {
+    getIt<SubscriptionService>().getPackages().then((value) {
+      if (value.hasError) {
+        Fluttertoast.showToast(msg: value.error ?? S.current.errorHappened);
+      } else if (value.isEmpty) {
+        Fluttertoast.showToast(msg: S.current.homeDataEmpty);
+      } else {
+        value as PackageModel;
+        _stateSubject.add(AboutStatePageOwner(
+          screenState,
+          value.data,
+          currentPage: 3
+        ));
+      }
+    });
   }
 }
