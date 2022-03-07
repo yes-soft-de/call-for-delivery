60d4c64ccd06be637d70459b9cdde59b468f8cb5
ralsalloum <ramisalloum12@hotmail.com>
2022-03-07 20:31:56 +0200

    createdAt field was added to notificationsLocal response

--- backend/src/Response/Notification/NotificationLocalResponse.php
+++ backend/src/Response/Notification/NotificationLocalResponse.php
@@ -4,9 +4,23 @@ namespace App\Response\Notification;
 
 class NotificationLocalResponse
 {
+    /**
+     * @var int|null
+     */
     public $id;
 
+    /**
+     * @var string|null
+     */
     public $title;
 
+    /**
+     * @var array|null
+     */
     public $message;
+
+    /**
+     * @var array|null
+     */
+    public $createdAt;
 }
