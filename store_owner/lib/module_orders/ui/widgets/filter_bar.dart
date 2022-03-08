8f761c682e5e63d72364a1d7ed69b35bb014eb9f
yazan99sh <syria.yazan19@gmail.com>
2022-03-08 17:04:49 +0200

    filter bar

---
+++ store_owner/lib/module_orders/ui/widgets/filter_bar.dart
@@ -0,0 +1,231 @@
+import 'package:flutter/material.dart';
+
+class FilterBar extends StatefulWidget {
+  /// current index of the selected item
+  final int currentIndex;
+
+  /// return the new index of selected item
+  final Function(int) onItemSelected;
+
+  /// the item list
+  final List<FilterItem> items;
+
+  /// color of the selected item content
+  final Color selectedContent;
+
+  /// color of the unselected item content
+  final Color unselectedContent;
+
+  /// duration of animation
+  final Duration animationDuration;
+
+  /// bar background color
+  final Color backgroundColor;
+
+  /// color of the item
+  final Color cursorColor;
+
+  /// radius of item card
+  final BorderRadius? cursorRadius;
+
+  /// spacing around edges
+  final bool floating;
+
+  /// border radius of bar
+  final BorderRadius? borderRadius;
+
+  /// padding
+  final EdgeInsets? padding;
+
+  /// height
+  final double? height;
+
+  FilterBar(
+      {required this.currentIndex,
+      required this.onItemSelected,
+      required this.items,
+      this.height,
+      required this.selectedContent,
+      required this.unselectedContent,
+      required this.animationDuration,
+      required this.backgroundColor,
+      required this.cursorColor,
+      this.cursorRadius,
+      this.floating = false,
+      this.borderRadius,
+      this.padding})
+      : assert(items.length >= 2) {
+    for (int i = 0; i < items.length; i++) {
+      items[i].index = i;
+    }
+  }
+
+  @override
+  _FilterBarState createState() => _FilterBarState();
+}
+
+class _FilterBarState extends State<FilterBar> {
+  late int currentIndex;
+  late List<FilterItem> items;
+  late Color selectedContent;
+  late Color unselectedContent;
+  late Duration animationDuration;
+  late Color backgroundColor;
+  late Color cursorColor;
+  BorderRadius? cursorRadius;
+  late bool floating;
+  double? opacity;
+  BorderRadius? borderRadius;
+  EdgeInsets? padding;
+  List<GlobalKey> _keys = [];
+  Offset? initialOffset;
+  late Widget itemBuilder;
+  late bool firstUse;
+  late double off;
+  bool end = true;
+  @override
+  void initState() {
+    currentIndex = widget.currentIndex;
+    items = widget.items;
+    selectedContent = widget.selectedContent;
+    unselectedContent = widget.unselectedContent;
+    animationDuration = widget.animationDuration;
+    backgroundColor = widget.backgroundColor;
+    cursorColor = widget.cursorColor;
+    cursorRadius = widget.cursorRadius;
+    floating = widget.floating;
+    borderRadius = widget.borderRadius;
+    padding = widget.padding;
+    firstUse = true;
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      try {
+        if (firstUse) {
+          _keys.forEach((element) {
+            itemSize.add(element.currentContext?.size ?? Size(0, 0));
+          });
+        }
+        RenderBox render =
+            _keys[currentIndex].currentContext?.findRenderObject() as RenderBox;
+        initialOffset = render.localToGlobal(Offset.fromDirection(1, -16));
+        if (initialOffset != null) {
+          initialOffset = Offset(initialOffset!.dx, initialOffset!.dx);
+        }
+        setState(() {});
+      } catch (e) {
+        print(e);
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPadding(
+      duration: animationDuration,
+      padding: padding ?? EdgeInsets.only(right: 8, left: 8.0),
+      child: AnimatedContainer(
+        height: widget.height ?? 75,
+        duration: animationDuration,
+        decoration: BoxDecoration(
+          color: backgroundColor.withOpacity(opacity ?? 1),
+          borderRadius: borderRadius,
+        ),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              AnimatedPositioned(
+                onEnd: () {
+                  end = true;
+                  setState(() {});
+                },
+                left: initialOffset?.dx ?? 0,
+                duration:
+                    firstUse ? Duration(milliseconds: 1) : animationDuration,
+                curve: Curves.easeInOut,
+                child: Container(
+                  height: itemSize.isEmpty ? 10 : itemSize[currentIndex].height,
+                  width: itemSize.isEmpty ? 10 : itemSize[currentIndex].width,
+                  decoration: BoxDecoration(
+                    color: cursorColor,
+                    gradient: LinearGradient(colors: [
+                      cursorColor.withOpacity(0.85),
+                      cursorColor.withOpacity(0.85),
+                      cursorColor.withOpacity(0.9),
+                      cursorColor.withOpacity(0.93),
+                      cursorColor.withOpacity(0.95),
+                      cursorColor,
+                    ]),
+                    borderRadius: cursorRadius ??
+                        BorderRadius.vertical(top: Radius.circular(18)),
+                  ),
+                ),
+              ),
+              Flex(
+                direction: Axis.horizontal,
+                children: getItemWidget(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> getItemWidget() {
+    List<Widget> barItems = [];
+    for (var element in items) {
+      _keys.add(GlobalKey(debugLabel: element.label));
+      barItems.add(
+        Expanded(
+          child: GestureDetector(
+              key: _keys[element.index],
+              onTap: () {
+                currentIndex = element.index;
+                end = false;
+                firstUse = false;
+                try {
+                  RenderBox render = _keys[element.index]
+                      .currentContext
+                      ?.findRenderObject() as RenderBox;
+                  initialOffset =
+                      render.localToGlobal(Offset.fromDirection(1, -16));
+                  if (initialOffset != null) {
+                    initialOffset =
+                        Offset(initialOffset!.dx, initialOffset!.dx);
+                  }
+                } catch (e) {
+                  print(e);
+                }
+                widget.onItemSelected(currentIndex);
+                setState(() {});
+              },
+              child: Center(
+                child: Text(
+                  element.label,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: currentIndex == element.index && end
+                        ? selectedContent
+                        : unselectedContent,
+                  ),
+                  textAlign: TextAlign.center,
+                  textScaleFactor: 1,
+                ),
+              )),
+        ),
+      );
+    }
+    return barItems;
+  }
+
+  List<Size> itemSize = [];
+}
+
+class FilterItem {
+  final String label;
+  late int index;
+
+  FilterItem({required this.label});
+}
