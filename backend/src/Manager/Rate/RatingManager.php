2b5e7788623bf14d7b8c7e3a6953c15ae1c5f17a
habibdevsy <alhabibcenter@hotmail.com>
2022-03-10 17:10:12 +0300

    createRatingByStore

---
+++ backend/src/Manager/Rate/RatingManager.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Manager\Rate;
+
+use App\AutoMapping;
+use App\Entity\RateEntity;
+use App\Repository\RateEntityRepository;
+use App\Request\Rate\RatingCreateRequest;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Manager\User\UserManager;
+
+
+class RatingManager
+{
+    private $autoMapping;
+    private $entityManager;
+    private $ratingRepository;
+    private $userManager;
+
+    public function __construct(AutoMapping $autoMapping, EntityManagerInterface $entityManager, RateEntityRepository $ratingRepository, UserManager $userManager)
+    {
+        $this->autoMapping = $autoMapping;
+        $this->entityManager = $entityManager;
+        $this->ratingRepository = $ratingRepository;
+        $this->userManager = $userManager;
+    }
+
+    public function createRating(RatingCreateRequest $request): ?RateEntity
+    {
+        $request->setRater($this->userManager->getUser($request->getRater()));
+        $request->setRated($this->userManager->getUser($request->getRated()));
+
+        $entity = $this->autoMapping->map(RatingCreateRequest::class, RateEntity::class, $request);
+       
+        $this->entityManager->persist($entity);
+
+        $this->entityManager->flush();
+
+        return $entity;
+    }
+}
---
+++ backend/src/Service/Rate/RatingService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service\Rate;
+
+use App\AutoMapping;
+use App\Entity\RateEntity;
+use App\Manager\Rate\RatingManager;
+use App\Request\Rate\RatingCreateRequest;
+use App\Response\Rate\RatingResponse;
+
+
+class RatingService
+{
+    private $autoMapping;
+    private $ratingManager;
+
+    public function __construct(AutoMapping $autoMapping, RatingManager $ratingManager)
+    {
+        $this->autoMapping = $autoMapping;
+        $this->ratingManager = $ratingManager;
+    }
+
+    public function createRating(RatingCreateRequest $request): ?RatingResponse
+    {
+        $rating = $this->ratingManager->createRating($request);
+
+        return $this->autoMapping->map(RateEntity::class, RatingResponse::class, $rating);
+    }
+}
