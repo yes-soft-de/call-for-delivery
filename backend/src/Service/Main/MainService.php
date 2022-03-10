d6c73c4b26ab16f2e81eaddbf9452c0f41092ec6
ralsalloum <ramisalloum12@hotmail.com>
2022-03-11 00:13:05 +0200

    createNotificationLocalBySuperAdmin function was added

--- backend/src/Service/Main/MainService.php
+++ backend/src/Service/Main/MainService.php
@@ -6,6 +6,7 @@ use App\AutoMapping;
 use App\Constant\Main\BackendHealthStatusConstant;
 use App\Constant\Notification\NotificationConstant;
 use App\Constant\Order\OrderResultConstant;
+use App\Constant\Order\OrderStateConstant;
 use App\Entity\OrderEntity;
 use App\Manager\Main\MainManager;
 use App\Request\Main\OrderStateUpdateBySuperAdminRequest;
@@ -68,7 +69,15 @@ class MainService
             return OrderResultConstant::ORDER_NOT_FOUND_RESULT;
 
         } else {
-            $this->notificationLocalService->createNotificationLocal($orderResult->getStoreOwner()->getStoreOwnerId(), NotificationConstant::UPDATE_ORDER_TITLE, NotificationConstant::UPDATE_ORDER_SUCCESS, $orderResult->getId());
+            $captainId = 0;
+
+            if($orderResult->getState() === OrderStateConstant::ORDER_STATE_DELIVERED) {
+                // Here we pass store owner id as captain id just until we have captain section set
+                $captainId = $orderResult->getStoreOwner()->getStoreOwnerId();
+            }
+
+            $this->notificationLocalService->createNotificationLocalBySuperAdmin($orderResult->getStoreOwner()->getStoreOwnerId(), NotificationConstant::UPDATE_ORDER_TITLE,
+                NotificationConstant::UPDATE_ORDER_SUCCESS, $orderResult->getState(), $captainId, $orderResult->getId());
 
             return $this->autoMapping->map(OrderEntity::class, OrderStateUpdateBySuperAdminResponse::class, $orderResult);
         }
