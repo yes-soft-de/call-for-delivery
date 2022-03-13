d5ab6ca6594484af71748d18a487182341229433
yazan99sh <syria.yazan19@gmail.com>
2022-03-13 21:38:36 +0200

    change label

--- store_owner/lib/module_orders/ui/screens/order_logs_screen.dart
+++ store_owner/lib/module_orders/ui/screens/order_logs_screen.dart
@@ -43,7 +43,8 @@ class OrderLogsScreenState extends State<OrderLogsScreen> {
     currentState = LoadingState(this);
     ordersFilter = FilterOrderRequest(
         state: 'pending',
-        fromDate: DateTime(today.year,today.month,today.day,0).toIso8601String(),
+        fromDate:
+            DateTime(today.year, today.month, today.day, 0).toIso8601String(),
         toDate: DateTime.now().toIso8601String());
     widget._stateManager.getOrdersFilters(this, ordersFilter);
     widget._stateManager.stateStream.listen((event) {
@@ -73,7 +74,9 @@ class OrderLogsScreenState extends State<OrderLogsScreen> {
             title: S.current.orderLog,
             actions: [
               CustomC4dAppBar.actionIcon(context, onTap: () {
-                ordersFilter.fromDate = DateTime(today.year,today.month,today.day,0).toIso8601String();
+                ordersFilter.fromDate =
+                    DateTime(today.year, today.month, today.day, 0)
+                        .toIso8601String();
                 ordersFilter.toDate = DateTime.now().toIso8601String();
                 currentIndex = 0;
                 ordersFilter.state = 'pending';
@@ -209,15 +212,18 @@ class OrderLogsScreenState extends State<OrderLogsScreen> {
               height: 40,
               cursorColor: Theme.of(context).colorScheme.primary,
               items: [
-                FilterItem(label: S.current.pendingOrders),
-                FilterItem(label: S.current.onGoingOrder),
-                FilterItem(label: S.current.completedOrders),
+                FilterItem(label: S.current.pending,),
+                FilterItem(label: S.current.ongoing),
+                FilterItem(label: S.current.completed),
+                FilterItem(label: S.current.cancelled2),
               ],
               onItemSelected: (index) {
                 if (index == 0) {
                   ordersFilter.state = 'pending';
                 } else if (index == 1) {
                   ordersFilter.state = 'ongoing';
+                } else if (index == 3) {
+                  ordersFilter.state = 'cancelled';
                 } else {
                   ordersFilter.state = 'delivered';
                 }
