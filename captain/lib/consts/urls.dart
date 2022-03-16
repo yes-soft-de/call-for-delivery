b00fbf462cb8d65cce7f595c46dad7ece00097eb
yazan99sh <syria.yazan19@gmail.com>
2022-03-16 18:27:29 +0200

    update routes

--- captain/lib/consts/urls.dart
+++ captain/lib/consts/urls.dart
@@ -1,5 +1,3 @@
-
-
 class Urls {
   static const String DOMAIN = 'http://134.209.241.49';
   static const String BASE_API = DOMAIN + '';
@@ -26,11 +24,12 @@ class Urls {
   static const NOTIFICATIONTOADMIN_API = BASE_API + '/notificationtoadmin';
   static const NOTIFICATIONNEWCHAT_ANYN_API =
       BASE_API + '/notificationnewchatanonymous';
-  static const CREATE_CAPTAIN_PROFILE = BASE_API + '/v1/captain/captainprofilupdate';
+  static const CREATE_CAPTAIN_PROFILE =
+      BASE_API + '/v1/captain/captainprofilupdate';
   static const CAPTAIN_ACTIVE_STATUS_API = BASE_API + '/captainisactive';
-  static const ORDER_STATUS_API = BASE_API + '/orderDetails/';
-  static const NEARBY_ORDERS_API = BASE_API + '/closestOrders';
-  static const CAPTAIN_ACCEPTED_ORDERS_API = BASE_API + '/getAcceptedOrder';
+  static const ORDER_STATUS_API = BASE_API + '/v1/order/captainorder/';
+  static const NEARBY_ORDERS_API = BASE_API + '/v1/order/closestorders';
+  static const CAPTAIN_ACCEPTED_ORDERS_API = BASE_API + '/v1/order/acceptedorder';
   static const ACCEPT_ORDER_API = BASE_API + '/acceptedOrder';
   static const CAPTAIN_ORDER_UPDATE_API = BASE_API + '/orderUpdateState';
   static const ORDER_UPDATE_BILL_API =
@@ -38,7 +37,8 @@ class Urls {
   static const UPDATES_API = BASE_API + '/updateall';
   static const ORDER_BY_ID = BASE_API + '/orderStatus/';
   static const SEND_TO_RECORD = BASE_API + '/record';
-  static const CAPTAIN_PROFILE_API = BASE_API + '/captainprofile';
+  static const CAPTAIN_PROFILE_API =
+      BASE_API + '/v1/captain/captainprofilebyid';
   static const TERMS_CAPTAIN = BASE_API + '/termscaptain';
   static const LOG_API = BASE_API + '/getRecords';
   static const CAPTAIN_BALANCE_ACCOUNT =
