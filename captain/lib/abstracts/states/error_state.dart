b016f809e9c64c19a533ccd63707b4905ed07fef
yazan99sh <syria.yazan19@gmail.com>
2022-03-16 18:26:34 +0200

    add new states

---
+++ captain/lib/abstracts/states/error_state.dart
@@ -0,0 +1,96 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:c4d/abstracts/states/state.dart';
+import 'package:c4d/generated/l10n.dart';
+import 'package:c4d/utils/components/custom_app_bar.dart';
+import 'package:c4d/utils/images/images.dart';
+
+class ErrorState extends States {
+  final String? error;
+  final List<String>? errors;
+  final String title;
+  final bool hasAppbar;
+  final VoidCallback onPressed;
+  final double? size;
+  final IconData? icon;
+  final VoidCallback? tapApp;
+  State<StatefulWidget> screenState;
+  ErrorState(this.screenState,
+      {this.error,
+      this.errors,
+      required this.onPressed,
+      required this.title,
+      this.hasAppbar = true,
+      this.size,
+      this.icon,
+      this.tapApp})
+      : super(screenState);
+
+  @override
+  Widget getUI(BuildContext context) {
+    return Scaffold(
+      appBar: hasAppbar
+          ? CustomC4dAppBar.appBar(
+              context,
+              title: title,
+              colorIcon: Theme.of(context).colorScheme.error,
+              icon: icon,
+              onTap: tapApp
+            )
+          : null,
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Flex(
+            direction: Axis.vertical,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Flushbar(
+                  title: S.of(context).thisErrorHappened,
+                  message: error ?? S.current.errorHappened,
+                  icon: const Icon(
+                    Icons.info,
+                    size: 28.0,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Theme.of(context).colorScheme.error,
+                  borderRadius: BorderRadius.circular(10),
+                  flushbarStyle: FlushbarStyle.FLOATING,
+                ),
+              ),
+              Container(
+                height: 24,
+              ),
+              SvgPicture.asset(
+                ImageAsset.ERROR_SVG,
+                height: size ?? MediaQuery.of(context).size.height * 0.5,
+              ),
+              Container(
+                height: 32,
+              ),
+              Center(
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).colorScheme.error,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          elevation: 0),
+                      onPressed: onPressed,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          S.of(context).refresh,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
