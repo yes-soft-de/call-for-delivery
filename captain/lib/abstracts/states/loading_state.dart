c172e7910f1ee2690194df347c7486243512f386
yazan99sh <syria.yazan19@gmail.com>
2022-03-19 17:47:54 +0200

    fix state

--- captain/lib/abstracts/states/loading_state.dart
+++ captain/lib/abstracts/states/loading_state.dart
@@ -1,13 +1,15 @@
+import 'package:c4d/utils/images/images.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:c4d/abstracts/states/state.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LoadingState extends States {
   LoadingState(State<StatefulWidget> screenState) : super(screenState);
   @override
   Widget getUI(BuildContext context) {
     return Center(
-      child: CircularProgressIndicator(),
+      child: LottieBuilder.asset(LottieAsset.LOADING_BOX),
     );
   }
 }
