7a7eb5066b501589db93546261a648acf5db3ba0
ralsalloum <ramisalloum12@hotmail.com>
2022-03-28 23:25:07 +0300

    api for purged database and insert fixtures data was added

--- backend/src/Controller/Commander/DataCommanderController.php
+++ backend/src/Controller/Commander/DataCommanderController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Commander;
 
 use App\Controller\BaseController;
+use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
@@ -27,6 +28,7 @@ class DataCommanderController extends BaseController
      * @IsGranted("ROLE_SUPER_ADMIN")
      * @param KernelInterface $kernel
      * @return Response
+     * @throws Exception
      */
     public function appendNewData(KernelInterface $kernel): Response
     {
@@ -50,29 +52,37 @@ class DataCommanderController extends BaseController
         return new Response($content);
     }
 
-//    /**
-//     * @Route("reenternewdata", name="reenterNewData", methods={"POST"})
-//     * @IsGranted("ROLE_SUPER_ADMIN")
-//     * @param KernelInterface $kernel
-//     * @return Response
-//     */
-//    public function createNewData(KernelInterface $kernel): Response
-//    {
-//        $application = new Application($kernel);
-//        $application->setAutoExit(false);
-//
-//        $input = new ArrayInput([
-//            'command' => 'hautelook:fixtures:load'
-//        ]);
-//
-//        // You can use NullOutput() if you don't need the output
-//        $output = new BufferedOutput();
-//        $application->run($input, $output);
-//
-//        // return the output, don't use if you used NullOutput()
-//        $content = $output->fetch();
-//
-//        // return new Response(""), if you used NullOutput()
-//        return new Response($content);
-//    }
+    /**
+     * @Route("reenternewdata", name="reenterNewData", methods={"POST"})
+     * @IsGranted("ROLE_SUPER_ADMIN")
+     * @param KernelInterface $kernel
+     * @return Response
+     * @throws Exception
+     */
+    public function createNewData(KernelInterface $kernel): Response
+    {
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'hautelook:fixtures:load'
+        ]);
+
+        /**
+         * running hautelook:fixtures:load asks the following question:
+         * Careful, database will be purged. Do you want to continue y/N ?
+         * So, to prevent this interaction (in order to run the command correctly) we have to set the following option
+         */
+        $input->setInteractive(false);
+
+        // You can use NullOutput() if you don't need the output
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        // return the output, don't use if you used NullOutput()
+        $content = $output->fetch();
+
+        // return new Response(""), if you used NullOutput()
+        return new Response($content);
+    }
 }
