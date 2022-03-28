c17fbdda121320b4a7162a29b8fbb249aabfa719
yazan99sh <syria.yazan19@gmail.com>
2022-03-28 20:06:22 +0300

    cleaning

--- store_owner/lib/module_profile/manager/profile/profile.manager.dart
+++ store_owner/lib/module_profile/manager/profile/profile.manager.dart
@@ -1,8 +1,5 @@
 import 'package:c4d/module_profile/repository/profile/profile.repository.dart';
-import 'package:c4d/module_profile/request/branch/create_branch_request.dart';
 import 'package:c4d/module_profile/request/profile/profile_request.dart';
-import 'package:c4d/module_profile/response/create_branch_response.dart';
-import 'package:c4d/module_profile/response/get_records_response.dart';
 import 'package:c4d/module_profile/response/profile_response/profile_response.dart';
 import 'package:c4d/utils/response/action_response.dart';
 import 'package:injectable/injectable.dart';
@@ -19,9 +16,5 @@ class ProfileManager {
       _repository.createOwnerProfile(profileRequest);
   Future<ActionResponse?> updateProfile(ProfileRequest profileRequest) =>
       _repository.updateProfile(profileRequest);
-
   Future<ProfileResponse?> getOwnerProfile() => _repository.getOwnerProfile();
-
-//  Future <List<Terms>> getTerms(UserRole role) => _repository.getTerms(role);
-
 }
