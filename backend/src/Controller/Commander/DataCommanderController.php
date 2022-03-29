8fcee5bab201fa01baed2e45aab92219f204926d
ralsalloum <ramisalloum12@hotmail.com>
2022-03-30 01:31:46 +0300

    renewdatabasefromscratch API was added

--- backend/src/Controller/Commander/DataCommanderController.php
+++ backend/src/Controller/Commander/DataCommanderController.php
@@ -3,11 +3,16 @@
 namespace App\Controller\Commander;
 
 use App\Controller\BaseController;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
 use Exception;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,9 +23,12 @@ use Symfony\Component\Serializer\SerializerInterface;
  */
 class DataCommanderController extends BaseController
 {
-    public function __construct(SerializerInterface $serializer)
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(SerializerInterface $serializer, EntityManagerInterface $entityManager)
     {
         parent::__construct($serializer);
+        $this->entityManager = $entityManager;
     }
 
     /**
@@ -84,5 +92,67 @@ class DataCommanderController extends BaseController
 
         // return new Response(""), if you used NullOutput()
         return new Response($content);
+    }
+
+    /**
+     * @Route("renewdatabasefromscratch", name="deleteAllDatabaseTablesAndInsertNewDate", methods={"POST"})
+     * @IsGranted("ROLE_SUPER_ADMIN")
+     *
+     * @OA\Tag(name="Eraser")
+     *
+     * @OA\Parameter(
+     *      name="token",
+     *      in="header",
+     *      description="token to be passed as a header",
+     *      required=true
+     * )
+     *
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns messege about successfully new schema was created",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code", example="201"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="object", property="Data",
+     *              @OA\Property(type="string", property="database"),
+     *              @OA\Property(type="string", property="content")
+     *          )
+     *      )
+     * )
+     *
+     * @Security(name="Bearer")
+     */
+    public function renewDataBaseFromScratch(KernelInterface $kernel): JsonResponse
+    {
+        $response = [];
+
+        // First, drop current schema and create a new one
+        $entitiesArray = $this->entityManager->getMetadataFactory()->getAllMetadata();
+
+        $tool = new SchemaTool($this->entityManager);
+
+        $tool->dropSchema($entitiesArray);
+        $tool->createSchema($entitiesArray);
+
+        if($tool) {
+            $response['database'] = "new schema was created";
+        }
+
+        // Second, insert fixtures data
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'hautelook:fixtures:load'
+        ]);
+
+        $input->setInteractive(false);
+
+        $output = new BufferedOutput();
+        $application->run($input, $output);
+
+        $response['content'] = $output->fetch();
+
+        return $this->response($response, self::CREATE);
     }
 }
