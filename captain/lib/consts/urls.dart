6746f0f5d6f2e0decd97613e28b88d8bad1f69e3
yazan99sh <syria.yazan19@gmail.com>
2022-04-04 15:53:00 +0300

    chat notification

--- captain/lib/consts/urls.dart
+++ captain/lib/consts/urls.dart
@@ -15,10 +15,10 @@ class Urls {
   static const UPDATE_PASSWORD = BASE_API + '/updatepassword';
   static const REPORT_API = BASE_API + '/report';
   static const CREATE_CUSTOM_PRODUCT_API = BASE_API + '/customproductnotfound';
-  static const NOTIFICATION_API = BASE_API + '/notificationtoken';
+  static const NOTIFICATION_API = DOMAIN + '/v1/notificationtoken/notificationtoken';
   static const COMPANYINFO_API = BASE_API + '/companyinfoforuser';
   static const NOTIFICATIONNEWCHAT_API =
-      BASE_API + '/notificationnewchatbyuserid';
+      BASE_API + '/v1/notificationfirbase/notificationnewchatbyuserid';
   static const NEEDFORSUPPORT = BASE_API + '/updateneedsupport';
   static const NEEDFORSUPPORT_ANYNAMOUS = BASE_API + '/anonymouschat';
   static const NOTIFICATIONTOADMIN_API = BASE_API + '/notificationtoadmin';
