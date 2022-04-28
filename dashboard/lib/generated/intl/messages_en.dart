4af78467c2b98c7a35057e890d1ccca4327b7498
yazan99sh <syria.yazan19@gmail.com>
2022-04-28 20:01:57 +0300

    update messages

--- dashboard/lib/generated/intl/messages_en.dart
+++ dashboard/lib/generated/intl/messages_en.dart
@@ -661,6 +661,7 @@ class MessageLookup extends MessageLookupByLibrary {
             "There is no profile data available"),
         "profileFetchedSuccessfully": MessageLookupByLibrary.simpleMessage(
             "Profile Data Fetched Successfully"),
+        "profit": MessageLookupByLibrary.simpleMessage("Profit"),
         "rateCaptain": MessageLookupByLibrary.simpleMessage("Rate Captain"),
         "rateCaptainMessage": MessageLookupByLibrary.simpleMessage(
             "Rate this captain upon your opinion"),
--- dashboard/lib/generated/l10n.dart
+++ dashboard/lib/generated/l10n.dart
@@ -6079,6 +6079,16 @@ class S {
       args: [],
     );
   }
+
+  /// `Profit`
+  String get profit {
+    return Intl.message(
+      'Profit',
+      name: 'profit',
+      desc: '',
+      args: [],
+    );
+  }
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<S> {
