e2c7a8b2c776186b23e6bb00418508bb6c7bebc7
yazan99sh <syria.yazan19@gmail.com>
2022-06-21 14:09:23 +0300

    click on notification

--- dashboard/lib/utils/helpers/firestore_helper.dart
+++ dashboard/lib/utils/helpers/firestore_helper.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:injectable/injectable.dart';
+import 'package:isolate_handler/isolate_handler.dart';
 
 @injectable
 class FireStoreHelper {
@@ -50,4 +54,42 @@ class FireStoreHelper {
       return;
     }
   }
+
+  Future<void> backgroundThread(String message) async {
+    if (Platform.isAndroid) {
+      final isolates = IsolateHandler();
+      try {
+        isolates.spawn(
+          entryPoint,
+          onReceive: (message) async {
+            await Firebase.initializeApp();
+            await FireStoreHelper().insertWatcher();
+            isolates.kill('FireStoreInserter');
+          },
+          onInitialized: () {
+            isolates.send(message, to: 'FireStoreInserter');
+          },
+          name: 'FireStoreInserter',
+        );
+      } catch (e) {
+        log(e.toString());
+      }
+    } else {
+      FireStoreHelper().insertWatcher().ignore();
+    }
+  }
+}
+
+void entryPoint(Map<String, dynamic> context) async {
+  // Calling initialize from the entry point with the context is
+  // required if communication is desired. It returns a messenger which
+  // allows listening and sending information to the main isolate.
+
+  final messenger = HandledIsolate.initialize(context);
+  // Triggered every time data is received from the main isolate.
+  messenger.listen((message) async {
+    // Add one to the count and send the new value back to the main
+    // isolate.
+    messenger.send(message);
+  });
 }
