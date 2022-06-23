d5b333e89d2dd0a401b4aeb5e42601c959050f6c
yazan99sh <syria.yazan19@gmail.com>
2022-06-23 17:40:53 +0300

    set a validator

--- store_owner/lib/utils/helpers/phone_number_detection.dart
+++ store_owner/lib/utils/helpers/phone_number_detection.dart
@@ -3,6 +3,9 @@ import 'package:the_country_number/the_country_number.dart';
 class PhoneNumberDetection {
   static String getPhoneNumber(String phone) {
     var result = '';
+    if (!isMobileNumberValid(phone.replaceAll(RegExp(r'[()-\s]'), ''))) {
+      return result;
+    }
     // remove white spaces
     result = phone.replaceAll(RegExp(r'[()-\s]'), '');
     if (result.length == 9) {
@@ -23,5 +26,16 @@ class PhoneNumberDetection {
       return result.substring(1);
     }
     return result;
+  }
+
+  static bool isMobileNumberValid(String phoneNumber) {
+    String regexPattern = r'^(?:[+0][1-9])?[0-9]{9,14}$';
+    var regExp =  RegExp(regexPattern);
+    if (phoneNumber.length == 0) {
+      return false;
+    } else if (regExp.hasMatch(phoneNumber)) {
+      return true;
+    }
+    return false;
   }
 }
