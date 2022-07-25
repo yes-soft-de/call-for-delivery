efe67335d49f7b1d5470c45ac8d1f48de38d4a7c
ralsalloum <ramisalloum12@hotmail.com>
2022-07-25 18:57:27 +0300

    deleteCaptainAccountAndProfileByAdmin function was updated

--- backend/src/Service/Eraser/CaptainEraserService.php
+++ backend/src/Service/Eraser/CaptainEraserService.php
@@ -23,6 +23,7 @@ use App\Service\Order\OrderService;
 use App\Service\ResetPassword\ResetPasswordOrderService;
 use App\Service\User\UserService;
 use App\Service\Verification\VerificationService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class CaptainEraserService
@@ -42,12 +43,13 @@ class CaptainEraserService
     private UserService $userService;
     private VerificationService $verificationService;
     private ResetPasswordOrderService $resetPasswordOrderService;
+    private EntityManagerInterface $entityManager;
 
     public function __construct(AutoMapping $autoMapping, ParameterBagInterface $param, OrderService $orderService, CaptainAmountFromOrderCashService $captainAmountFromOrderCashService,
                                 CaptainFinancialDuesService $captainFinancialDuesService, CaptainPaymentService $captainPaymentService, CaptainPaymentToCompanyService $captainPaymentToCompanyService,
                                 CaptainFinancialSystemDetailService $captainFinancialSystemDetailService, ImageService $imageService, NotificationFirebaseService $notificationFirebaseService,
                                 OrderChatRoomService $orderChatRoomService, CaptainService $captainService, UserService $userService, VerificationService $verificationService,
-                                ResetPasswordOrderService $resetPasswordOrderService)
+                                ResetPasswordOrderService $resetPasswordOrderService, EntityManagerInterface $entityManager)
     {
         $this->autoMapping = $autoMapping;
         $this->param = $param;
@@ -64,6 +66,7 @@ class CaptainEraserService
         $this->userService = $userService;
         $this->verificationService = $verificationService;
         $this->resetPasswordOrderService = $resetPasswordOrderService;
+        $this->entityManager = $entityManager;
     }
 
     public function deleteCaptainAccountAndProfileBySuperAdmin(DeleteCaptainAccountAndProfileBySuperAdminRequest $request): string|DeleteCaptainAccountAndProfileBySuperAdminResponse
@@ -138,66 +141,80 @@ class CaptainEraserService
 
     public function deleteCaptainAccountAndProfileByAdmin(DeleteCaptainAccountAndProfileByAdminRequest $request): string|DeleteCaptainAccountAndProfileByAdminResponse
     {
-        // Check if there are cash orders payments for the captain
-        $cashOrdersPayments = $this->captainAmountFromOrderCashService->getCashOrdersPaymentsByCaptainId($request->getCaptainId());
+        $this->entityManager->getConnection()->beginTransaction();
 
-        if (! empty($cashOrdersPayments)) {
-            return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_CASH_ORDER_PAYMENTS;
+        try {
+            // Check if there are cash orders payments for the captain
+            $cashOrdersPayments = $this->captainAmountFromOrderCashService->getCashOrdersPaymentsByCaptainId($request->getCaptainId());
+
+            if (!empty($cashOrdersPayments)) {
+                return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_CASH_ORDER_PAYMENTS;
+            }
+
+            // check if there are financial dues for the captain
+            $financialDues = $this->captainFinancialDuesService->getFinancialDuesByCaptainId($request->getCaptainId());
+
+            if (!empty($financialDues)) {
+                return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_FINANCIAL_DUES;
+            }
+
+            // check if there are payments for the captain
+            $payments = $this->captainPaymentService->getPaymentsByCaptainId($request->getCaptainId());
+
+            if (!empty($payments)) {
+                return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_PAYMENTS;
+            }
+
+            // check if there are payments to the company for the captain
+            $paymentsToCompany = $this->captainPaymentToCompanyService->getPaymentToCompanyByCaptainId($request->getCaptainId());
+
+            if (!empty($paymentsToCompany)) {
+                return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_PAYMENTS_TO_COMPANY;
+            }
+
+            // check if there is no orders
+            $orders = $this->orderService->getOrdersByCaptainId($request->getCaptainId());
+
+            if (!empty($orders)) {
+                return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_ORDERS;
+            }
+
+            // Now we can start deleting the account and related info
+            // delete financial system that the captain subscribed with
+            $this->captainFinancialSystemDetailService->deleteCaptainFinancialSystemDetailsByCaptainId($request->getCaptainId());
+
+            // delete all captain profile images
+            $this->imageService->deleteCaptainProfileImagesByCaptainId($request->getCaptainId());
+
+            // delete notification firebase token
+            $this->notificationFirebaseService->deleteTokenByUserAndAppType($request->getCaptainId(), NotificationTokenConstant::APP_TYPE_CAPTAIN);
+
+            // delete order chat rooms
+            $this->orderChatRoomService->deleteOrderChatRoomEntitiesByCaptainId($request->getCaptainId());
+
+            // delete captain profile
+            $this->captainService->deleteCaptainProfileByCaptainId($request->getCaptainId());
+
+            // delete related verification records
+            $this->verificationService->deleteAllVerificationCodesByIdOfUser($request->getCaptainId());
+
+            // delete reset password requests
+            $this->resetPasswordOrderService->deleteAllResetPasswordOrdersByUserId($request->getCaptainId());
+
+            $captainProfile = $this->captainService->getCaptainProfileByUserId($request->getCaptainId());
+
+            if ($captainProfile === null) {
+                // finally, delete captain user record as long as his/her profile was deleted
+                $userResult = $this->userService->deleteUserById($request->getCaptainId());
+            }
+
+            $this->entityManager->getConnection()->commit();
+
+            return $this->autoMapping->map(UserEntity::class, DeleteCaptainAccountAndProfileByAdminResponse::class, $userResult);
+
+        } catch (\Exception $e) {
+            $this->entityManager->getConnection()->rollBack();
+            throw $e;
         }
-
-        // check if there are financial dues for the captain
-        $financialDues = $this->captainFinancialDuesService->getFinancialDuesByCaptainId($request->getCaptainId());
-
-        if (! empty($financialDues)) {
-            return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_FINANCIAL_DUES;
-        }
-
-        // check if there are payments for the captain
-        $payments = $this->captainPaymentService->getPaymentsByCaptainId($request->getCaptainId());
-
-        if (! empty($payments)) {
-            return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_PAYMENTS;
-        }
-
-        // check if there are payments to the company for the captain
-        $paymentsToCompany = $this->captainPaymentToCompanyService->getPaymentToCompanyByCaptainId($request->getCaptainId());
-
-        if (! empty($paymentsToCompany)) {
-            return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_PAYMENTS_TO_COMPANY;
-        }
-
-        // check if there is no orders
-        $orders = $this->orderService->getOrdersByCaptainId($request->getCaptainId());
-
-        if (! empty($orders)) {
-            return EraserResultConstant::CAN_NOT_DELETE_USER_HAS_ORDERS;
-        }
-
-        // Now we can start deleting the account and related info
-        // delete financial system that the captain subscribed with
-        $this->captainFinancialSystemDetailService->deleteCaptainFinancialSystemDetailsByCaptainId($request->getCaptainId());
-
-        // delete all captain profile images
-        $this->imageService->deleteCaptainProfileImagesByCaptainId($request->getCaptainId());
-
-        // delete notification firebase token
-        $this->notificationFirebaseService->deleteTokenByUserAndAppType($request->getCaptainId(), NotificationTokenConstant::APP_TYPE_CAPTAIN);
-
-        // delete order chat rooms
-        $this->orderChatRoomService->deleteOrderChatRoomEntitiesByCaptainId($request->getCaptainId());
-
-        // delete captain profile
-        $this->captainService->deleteCaptainProfileByCaptainId($request->getCaptainId());
-
-        // delete related verification records
-        $this->verificationService->deleteAllVerificationCodesByIdOfUser($request->getCaptainId());
-
-        // delete reset password requests
-        $this->resetPasswordOrderService->deleteAllResetPasswordOrdersByUserId($request->getCaptainId());
-
-        // finally, delete captain user record
-        $userResult = $this->userService->deleteUserById($request->getCaptainId());
-
-        return $this->autoMapping->map(UserEntity::class, DeleteCaptainAccountAndProfileByAdminResponse::class, $userResult);
     }
 }
