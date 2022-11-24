419487e242a6df3f39d186f5d59cab14bcd9cc4e
yazan99sh <syria.yazan19@gmail.com>
2022-11-24 20:26:31 +0400

    fix order without distance bug

--- dashboard/lib/module_orders/ui/state/order_without_distance/order_without_distance_loaded_state.dart
+++ dashboard/lib/module_orders/ui/state/order_without_distance/order_without_distance_loaded_state.dart
@@ -7,6 +7,7 @@ import 'package:c4d/module_orders/response/orders_response/destination.dart';
 import 'package:c4d/module_orders/ui/screens/orders_without_distance_screen.dart';
 import 'package:c4d/module_orders/ui/widgets/geo_widget.dart';
 import 'package:c4d/module_orders/ui/widgets/orders_without_distance/order_without_distance_card.dart';
+import 'package:c4d/module_orders/ui/widgets/orders_without_distance/update_distance_dialog.dart';
 import 'package:c4d/utils/components/custom_feild.dart';
 import 'package:c4d/utils/components/custom_list_view.dart';
 import 'package:c4d/utils/helpers/order_status_helper.dart';
@@ -46,105 +47,23 @@ class OrderWithoutDistanceLoadedState extends States {
               branchLocation: element.branchLocation ?? LatLng(0, 0),
               destinationUrl: element.destinationLink ?? '',
               onEdit: () {
-                var _kilometer = TextEditingController();
-                num? distance = null;
                 showDialog(
                     context: context,
                     builder: (ctx) {
-                      LatLng? coordinations;
-                      return StatefulBuilder(builder: (context, setState) {
-                        return AlertDialog(
-                          scrollable: true,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(25)),
-                          title: Text(S.current.updateDistance),
-                          content: Container(
-                            child: Column(
-                              children: [
-                                ListTile(
-                                  title: Text(
-                                      S.current.provideClientCoordinations),
-                                  subtitle: CustomFormField(
-                                    onChanged: () {
-                                      var coord = _kilometer.text.split(',');
-                                      if (coord.length == 2) {
-                                        coordinations = LatLng(
-                                            double.tryParse(coord[0].trim()) ??
-                                                0,
-                                            double.tryParse(coord[1].trim()) ??
-                                                0);
-                                      }
-                                      setState(() {});
-                                    },
-                                    controller: _kilometer,
-                                    hintText: S
-                                        .current.provideClientCoordinationsHint,
-                                  ),
-                                ),
-                                Visibility(
-                                    visible: coordinations != null,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(16.0),
-                                      child: Container(
-                                        width: double.maxFinite,
-                                        decoration: BoxDecoration(
-                                            borderRadius:
-                                                BorderRadius.circular(25),
-                                            color: Colors.amber),
-                                        child: Padding(
-                                            padding: const EdgeInsets.all(12.0),
-                                            child: GeoDistanceText(
-                                              destination:
-                                                  coordinations ?? LatLng(0, 0),
-                                              origin: element.branchLocation ??
-                                                  LatLng(0, 0),
-                                              destance: (d, cost) {
-                                                distance =
-                                                    num.tryParse(d ?? '');
-                                                setState(() {});
-                                              },
-                                              storeID: -1,
-                                            )),
-                                      ),
-                                    )),
-                              ],
-                            ),
-                          ),
-                          actionsAlignment: MainAxisAlignment.center,
-                          actions: [
-                            ElevatedButton(
-                                onPressed: distance == null ||
-                                        coordinations == null
-                                    ? null
-                                    : () {
-                                        Navigator.of(ctx).pop();
-                                        screenState.updateDistance(
-                                            UpdateDistanceRequest(
-                                          id: element.id,
-                                          storeBranchToClientDistance: distance,
-                                          destination: Destination(
-                                              lat: coordinations?.latitude,
-                                              lon: coordinations?.longitude,
-                                              link: element.destinationLink),
-                                        ));
-                                        _kilometer.clear();
-                                      },
-                                child: Text(
-                                  S.current.confirm,
-                                  style: Theme.of(context).textTheme.button,
-                                )),
-                            ElevatedButton(
-                                onPressed: () {
-                                  Navigator.of(ctx).pop();
-                                  _kilometer.clear();
-                                },
-                                child: Text(
-                                  S.current.cancel,
-                                  style: Theme.of(context).textTheme.button,
-                                ))
-                          ],
-                        );
-                      });
+                      return AlertDialog(
+                        scrollable: true,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25)),
+                        title: Text(S.current.updateDistance),
+                        content: UpdateDistanceDialog(
+                          branchLocation:
+                              element.branchLocation ?? LatLng(0, 0),
+                          callback: (request) {
+                            screenState.updateDistance(request);
+                          },
+                          id: element.id,
+                        ),
+                      );
                     });
               },
             ),
