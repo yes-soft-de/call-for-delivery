e9b6ed09672598b84cd6afe2f1d670f94697473d
ralsalloum <ramisalloum12@hotmail.com>
2022-12-20 17:06:57 +0200

    waiting time before update order state by captain had been changed

---
+++ backend/src/Constant/Order/OrderUpdateStateConstant.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Constant\Order;
+
+final class OrderUpdateStateConstant
+{
+    // waiting minutes before updating order state by captain
+    const ORDER_STATE_UPDATE_AFTER_TIME_CONST = 2;
+}
