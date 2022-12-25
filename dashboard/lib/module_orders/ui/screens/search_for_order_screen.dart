0af7c7b8b3aceef85895281f8f60ca994bbb1fbb
yazan99sh <syria.yazan19@gmail.com>
2022-12-25 22:21:13 +0400

    search for order

--- dashboard/lib/module_orders/ui/screens/search_for_order_screen.dart
+++ dashboard/lib/module_orders/ui/screens/search_for_order_screen.dart
@@ -1,17 +1,14 @@
 import 'dart:async';
 import 'package:c4d/abstracts/states/loading_state.dart';
 import 'package:c4d/abstracts/states/state.dart';
-import 'package:c4d/di/di_config.dart';
 import 'package:c4d/module_orders/request/order_filter_request.dart';
 import 'package:c4d/module_orders/state_manager/search_for_order_state_manager.dart';
 import 'package:c4d/module_orders/ui/widgets/filter_bar.dart';
-import 'package:c4d/module_theme/pressistance/theme_preferences_helper.dart';
-import 'package:c4d/utils/components/custom_app_bar.dart';
+import 'package:c4d/utils/components/custom_feild.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:c4d/generated/l10n.dart';
 import 'package:c4d/module_auth/authorization_routes.dart';
-import 'package:intl/intl.dart';
 
 @injectable
 class SearchForOrderScreen extends StatefulWidget {
@@ -27,7 +24,7 @@ class SearchForOrderScreenState extends State<SearchForOrderScreen> {
   late States currentState;
   int currentIndex = 0;
   StreamSubscription? _stateSubscription;
-
+  final searchForOrderController = TextEditingController();
   void refresh() {
     if (mounted) {
       setState(() {});
@@ -44,7 +41,7 @@ class SearchForOrderScreenState extends State<SearchForOrderScreen> {
   void initState() {
     super.initState();
     currentState = LoadingState(this);
-    ordersFilter = FilterOrderRequest(orderId: '');
+    ordersFilter = FilterOrderRequest();
     widget._stateManager.getOrdersFilters(this, ordersFilter);
     _stateSubscription = widget._stateManager.stateStream.listen((event) {
       currentState = event;
@@ -75,6 +72,13 @@ class SearchForOrderScreenState extends State<SearchForOrderScreen> {
         }
       },
       child: Scaffold(
+        appBar: AppBar(
+          title: CustomFormField(
+            hintText: S.current.searchForOrder,
+            controller: searchForOrderController,
+            numbers: true,
+          ),
+        ),
         body: Column(
           children: [
             SizedBox(
