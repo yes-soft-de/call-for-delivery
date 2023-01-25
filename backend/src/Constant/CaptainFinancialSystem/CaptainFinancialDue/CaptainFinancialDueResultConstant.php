f7c8942e7ef67b38058f8d4f62c6ee0e92c6d7b7
ralsalloum <ramisalloum12@hotmail.com>
2023-01-26 00:38:08 +0200

    delete captain account had been updated

---
+++ backend/src/Constant/CaptainFinancialSystem/CaptainFinancialDue/CaptainFinancialDueResultConstant.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Constant\CaptainFinancialSystem\CaptainFinancialDue;
+
+final class CaptainFinancialDueResultConstant
+{
+    const CAPTAIN_FINANCIAL_DUE_HAVE_ZERO_VALUE_CONST = 164;
+}
