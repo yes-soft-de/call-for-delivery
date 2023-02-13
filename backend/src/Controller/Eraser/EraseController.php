1ac82f2a91d5e7a41fccb343877fba66bff06c9d
ralsalloum <ramisalloum12@hotmail.com>
2023-02-14 00:03:37 +0200

    reset data API had been commented out

--- backend/src/Controller/Eraser/EraseController.php
+++ backend/src/Controller/Eraser/EraseController.php
@@ -41,46 +41,46 @@ class EraseController extends BaseController
         $this->eraserService = $eraserService;
     }
 
-    /**
-     * @Route("dropalltables", name="deleteAllDatabaseTables", methods={"DELETE"})
-     * @IsGranted("ROLE_SUPER_ADMIN")
-     *
-     * @OA\Tag(name="Eraser")
-     *
-     * @OA\Parameter(
-     *      name="token",
-     *      in="header",
-     *      description="token to be passed as a header",
-     *      required=true
-     * )
-     *
-     * @OA\Response(
-     *      response=200,
-     *      description="Returns all data tables were being re-created message",
-     *      @OA\JsonContent(
-     *          @OA\Property(type="string", property="status_code", example="401"),
-     *          @OA\Property(type="string", property="msg"),
-     *          @OA\Property(type="string", property="Data", example="All data tables were being re-created"
-     *          )
-     *      )
-     * )
-     *
-     * @Security(name="Bearer")
-     */
-    public function reCreateNewDatabaseScheme(): JsonResponse
-    {
-        $entitiesArray = $this->entityManager->getMetadataFactory()->getAllMetadata();
-
-        $tool = new SchemaTool($this->entityManager);
-
-        $tool->dropSchema($entitiesArray);
-        $tool->createSchema($entitiesArray);
-
-        if($tool)
-        {
-            return $this->response("ِAll data tables were being re-created", self::DELETE);
-        }
-    }
+//    /**
+//     * @Route("dropalltables", name="deleteAllDatabaseTables", methods={"DELETE"})
+//     * @IsGranted("ROLE_SUPER_ADMIN")
+//     *
+//     * @OA\Tag(name="Eraser")
+//     *
+//     * @OA\Parameter(
+//     *      name="token",
+//     *      in="header",
+//     *      description="token to be passed as a header",
+//     *      required=true
+//     * )
+//     *
+//     * @OA\Response(
+//     *      response=200,
+//     *      description="Returns all data tables were being re-created message",
+//     *      @OA\JsonContent(
+//     *          @OA\Property(type="string", property="status_code", example="401"),
+//     *          @OA\Property(type="string", property="msg"),
+//     *          @OA\Property(type="string", property="Data", example="All data tables were being re-created"
+//     *          )
+//     *      )
+//     * )
+//     *
+//     * @Security(name="Bearer")
+//     */
+//    public function reCreateNewDatabaseScheme(): JsonResponse
+//    {
+//        $entitiesArray = $this->entityManager->getMetadataFactory()->getAllMetadata();
+//
+//        $tool = new SchemaTool($this->entityManager);
+//
+//        $tool->dropSchema($entitiesArray);
+//        $tool->createSchema($entitiesArray);
+//
+//        if($tool)
+//        {
+//            return $this->response("ِAll data tables were being re-created", self::DELETE);
+//        }
+//    }
 
     /**
      * super admin: delete all bid orders and their images and prices offers
