7fe943c4fe469025a39b65aa358c448542a301d9
mehsen alk <mehsen2222@gmail.com>
2023-04-12 06:05:01 -0700

    delete unused method

--- captain/lib/module_notifications/preferences/notification_preferences/notification_preferences.dart
+++ captain/lib/module_notifications/preferences/notification_preferences/notification_preferences.dart
@@ -8,7 +8,6 @@ class NotificationsPrefHelper {
   var box = Hive.box('Notifications');
   // ignore: non_constant_identifier_names
   final NEW_NOTIFICATION = 'new_notifications';
-  static const kNewMessageFromSupport = 'new_message_from_support';
 
   void setNotificationPath(String ringtone) {
     box.put('Ringtone', ringtone);
@@ -51,24 +50,5 @@ class NotificationsPrefHelper {
 
   void clearNewLocalNotifications() {
     box.delete(NEW_NOTIFICATION);
-  }
-
-  void setNewMessageFromSupport() {
-    if (getNewMessageFromSupport() == null) {
-      box.put(
-          kNewMessageFromSupport,
-          DateTime.now()
-              .subtract(const Duration(minutes: 2))
-              .toIso8601String());
-      getIt<GlobalStateManager>().update();
-    }
-  }
-
-  String? getNewMessageFromSupport() {
-    return box.get(kNewMessageFromSupport);
-  }
-
-  void clearNewMessageFromSupport() {
-    box.delete(kNewMessageFromSupport);
   }
 }
