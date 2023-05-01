cabc77b9238c0f168ad3f64b9cc875a3686659c2
ralsalloum <rami.alsalloum@hotmail.com>
2023-05-01 23:47:47 +0300

    filterstoreduesumfromcashorderbyadmin API had been added

--- backend/src/Controller/Admin/StoreOwnerDuesFromCashOrders/AdminStoreOwnerDuesFromCashOrdersController.php
+++ backend/src/Controller/Admin/StoreOwnerDuesFromCashOrders/AdminStoreOwnerDuesFromCashOrdersController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin\StoreOwnerDuesFromCashOrders;
 
 use App\AutoMapping;
 use App\Controller\BaseController;
+use App\Request\Admin\StoreOwnerDuesFromCashOrders\StoreDueSumFromCashOrderFilterByAdminRequest;
 use App\Service\Admin\StoreOwnerDuesFromCashOrders\AdminStoreOwnerDuesFromCashOrdersService;;
 use Nelmio\ApiDocBundle\Annotation\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -18,23 +19,20 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 use App\Request\Admin\StoreOwnerDuesFromCashOrders\StoreOwnerDuesFromCashOrdersFilterGetRequest;
 use Nelmio\ApiDocBundle\Annotation\Model;
 
-
 /**
  * admin :fetch Store Owner Dues From Cash Orders.
  * @Route("v1/admin/storeownerduesfromcashorders/")
  */
 class AdminStoreOwnerDuesFromCashOrdersController extends BaseController
 {
-    private AdminStoreOwnerDuesFromCashOrdersService $adminStoreOwnerDuesFromCashOrdersService;
-    private AutoMapping $autoMapping;
-    private ValidatorInterface $validator;
-
-    public function __construct(SerializerInterface $serializer, AdminStoreOwnerDuesFromCashOrdersService $adminStoreOwnerDuesFromCashOrdersService, AutoMapping $autoMapping, ValidatorInterface $validator)
+    public function __construct(
+        SerializerInterface $serializer,
+        private AdminStoreOwnerDuesFromCashOrdersService $adminStoreOwnerDuesFromCashOrdersService,
+        private AutoMapping $autoMapping,
+        private ValidatorInterface $validator
+    )
     {
         parent::__construct($serializer);
-        $this->autoMapping = $autoMapping;
-        $this->validator = $validator;
-        $this->adminStoreOwnerDuesFromCashOrdersService = $adminStoreOwnerDuesFromCashOrdersService;
     }
 
     /**
@@ -51,7 +49,7 @@ class AdminStoreOwnerDuesFromCashOrdersController extends BaseController
      *      description="token to be passed as a header",
      *      required=true
      * )
-     * 
+     *
      * @OA\RequestBody(
      *      description="get Store Owner Dues From Cash Orders By StoreId and Date",
      *      @OA\JsonContent(
@@ -104,6 +102,63 @@ class AdminStoreOwnerDuesFromCashOrdersController extends BaseController
 
         $result = $this->adminStoreOwnerDuesFromCashOrdersService->filterStoreOwnerDuesFromCashOrders($request);
         
+        return $this->response($result, self::FETCH);
+    }
+
+    /**
+     * admin: filter store due sum from cash orders
+     * @Route("filterstoreduesumfromcashorderbyadmin", name="filterStoreDueSumFromCashOrderByAdmin", methods={"POST"})
+     * @IsGranted("ROLE_ADMIN")
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @OA\Tag(name="Store Owner Dues From Cash Orders")
+     *
+     * @OA\Parameter(
+     *      name="token",
+     *      in="header",
+     *      description="token to be passed as a header",
+     *      required=true
+     * )
+     *
+     * @OA\RequestBody(
+     *      description="filter request options",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="integer", property="isPaid")
+     *      )
+     * )
+     *
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns Store Owner Due Sum from cash orders",
+     *      @OA\JsonContent(
+     *          @OA\Property(type="string", property="status_code"),
+     *          @OA\Property(type="string", property="msg"),
+     *          @OA\Property(type="array", property="Data",
+     *              @OA\Items(
+     *                  @OA\Property(type="integer", property="id"),
+     *                  @OA\Property(type="integer", property="storeOwnerProfileId"),
+     *                  @OA\Property(type="string", property="storeOwnerName"),
+     *                  @OA\Property(type="array", property="image",
+     *                      @OA\Items()
+     *                  ),
+     *                  @OA\Property(type="number", property="amountSum"),
+     *                  @OA\Property(type="number", property="toBePaid"),
+     *              )
+     *          )
+     *      )
+     * )
+     *
+     * @Security(name="Bearer")
+     */
+    public function filterStoreDueFromCashOrdersByAdmin(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $request = $this->autoMapping->map(stdClass::class, StoreDueSumFromCashOrderFilterByAdminRequest::class, (object)$data);
+
+        $result = $this->adminStoreOwnerDuesFromCashOrdersService->filterStoreDueFromCashOrdersByAdmin($request);
+
         return $this->response($result, self::FETCH);
     }
 }
