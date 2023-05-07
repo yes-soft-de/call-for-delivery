36797f694d36b45a5be6e7a701ca5a1147dfa51f
ralsalloum <rami.alsalloum@hotmail.com>
2023-05-07 20:19:20 +0300

    fetchintvalue api had been added

---
+++ backend/src/Service/Maintainer/MaintainerService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service\Maintainer;
+
+class MaintainerService
+{
+    public function __construct()
+    {
+    }
+
+    /**
+     * Returns only integer single value
+     */
+    public function getIntValue(): int
+    {
+        return 200;
+    }
+}
