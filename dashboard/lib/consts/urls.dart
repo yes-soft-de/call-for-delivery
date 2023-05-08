d29465d7cffa07f5bc2c7ceed26fec50cefddecb
Yazan99sh <yazan.shekh.mohammed@gmail.com>
2023-05-08 21:43:59 +0400

    update version

--- dashboard/lib/consts/urls.dart
+++ dashboard/lib/consts/urls.dart
@@ -1,5 +1,5 @@
 // Developing Domain ===> 'http://134.209.241.49';
-// Production Domain ===> 'http://134.209.241.49';
+// Production Domain ===> 'http://46.101.100.62';
 class Urls {
   static List<String> admins = [
     '551111111',
@@ -10,7 +10,7 @@ class Urls {
     '552222225',
   ];
   /*--------BASES-------------------*/
-  static const String DOMAIN = 'http://134.209.241.49';
+  static const String DOMAIN = 'http://46.101.100.62';
   static const String BASE_API = DOMAIN + '';
   static const String VERSION = '/v1';
   static const GEO_DISTANCE = DOMAIN + '/v1/geodistance/geodistance';
