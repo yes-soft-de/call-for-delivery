9ceebbb44aeb394d84d4ea0e24a425cc68c5c61e
Yazan99sh <yazan.shekh.mohammed@gmail.com>
2023-05-20 22:29:28 +0400

    add toast on web

--- dashboard/lib/utils/helpers/custom_flushbar.dart
+++ dashboard/lib/utils/helpers/custom_flushbar.dart
@@ -3,9 +3,10 @@ import 'package:c4d/generated/l10n.dart';
 import 'package:c4d/global_nav_key.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class CustomFlushBarHelper {
-static void createSuccess(
+  static void createSuccess(
       {required String title,
       required String message,
       int timeout = 4,
@@ -13,51 +14,61 @@ static void createSuccess(
       bool top = false,
       EdgeInsets? padding,
       EdgeInsets? margin}) {
-     var context = GlobalVariable.navState.currentContext!;
-     Flushbar(
-      maxWidth: 600,
-      title: title,
-      message: message,
-      icon: Icon(
-        Icons.info,
-        size: 28.0,
-        color: Colors.white,
-      ),
-      backgroundColor: background ?? Colors.green,
-      duration: Duration(seconds: timeout),
-      borderRadius: BorderRadius.circular(25),
-      flushbarStyle: FlushbarStyle.FLOATING,
-      margin: margin ?? EdgeInsets.all(8),
-      padding: padding ?? EdgeInsets.all(16),
-      flushbarPosition: top ? FlushbarPosition.TOP : FlushbarPosition.BOTTOM,
-    ).show(context);
+    if (kIsWeb) {
+      Fluttertoast.showToast(
+          msg: message, backgroundColor: Colors.green, textColor: Colors.white);
+    } else {
+      var context = GlobalVariable.navState.currentContext!;
+      Flushbar(
+        maxWidth: 600,
+        title: title,
+        message: message,
+        icon: Icon(
+          Icons.info,
+          size: 28.0,
+          color: Colors.white,
+        ),
+        backgroundColor: background ?? Colors.green,
+        duration: Duration(seconds: timeout),
+        borderRadius: BorderRadius.circular(25),
+        flushbarStyle: FlushbarStyle.FLOATING,
+        margin: margin ?? EdgeInsets.all(8),
+        padding: padding ?? EdgeInsets.all(16),
+        flushbarPosition: top ? FlushbarPosition.TOP : FlushbarPosition.BOTTOM,
+      ).show(context);
+    }
   }
 
   static void createError(
       {required String title, required String message, int timeout = 4}) {
-     var context = GlobalVariable.navState.currentContext!;
-     Flushbar(
-      maxWidth: 600,
-      title: title,
-      message: message,
-      icon: Icon(
-        Icons.info,
-        size: 28.0,
-        color: Colors.white,
-      ),
-      backgroundColor: Colors.red,
-      duration: Duration(seconds: timeout),
-      borderRadius: BorderRadius.circular(25),
-      flushbarStyle: FlushbarStyle.FLOATING,
-      margin: EdgeInsets.all(8),
-    ).show(context);
+    if (kIsWeb) {
+      Fluttertoast.showToast(
+          msg: message, backgroundColor: Colors.red, textColor: Colors.white);
+    } else {
+      var context = GlobalVariable.navState.currentContext!;
+      Flushbar(
+        maxWidth: 600,
+        title: title,
+        message: message,
+        icon: Icon(
+          Icons.info,
+          size: 28.0,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.red,
+        duration: Duration(seconds: timeout),
+        borderRadius: BorderRadius.circular(25),
+        flushbarStyle: FlushbarStyle.FLOATING,
+        margin: EdgeInsets.all(8),
+      ).show(context);
+    }
   }
 
   static showSnackSuccess(State screenState, String message, bool loading) {
     if (loading) {
       createSuccess(
-              title: S.current.warnning,
-              message: S.current.storeUpdatedSuccessfully);
+          title: S.current.warnning,
+          message: S.current.storeUpdatedSuccessfully);
     } else {
       Fluttertoast.showToast(msg: message);
     }
