8916804b7c6d9992afda913867afff0e1285d218
Yazan99sh <yazan.shekh.mohammed@gmail.com>
2023-06-15 22:01:19 +0400

    update git ignore

--- store_owner/lib/consts/secret_payments_keys.dart
+++
@@ -1,6 +0,0 @@
-class SecretPaymentsKeys {
-  static String production = 'sk_live_PDQ9wF4CvtUAXasldONHEjgu';
-  static String productionIOS = 'sk_live_XvsR9mYEoHZBzT6Ubi8KSV0g';
-  static String sandBox = 'sk_test_2YFEnUkl3KZ94Jo7Q8fMG0Ih';
-  static String sandBoxIOS = 'sk_test_FUnKONieoSJw823Im1vuz4PL';
-}
