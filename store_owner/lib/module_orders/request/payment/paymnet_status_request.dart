a354ae05153842128abf9b04fb24061a5b9e4386
mehsen alk <mehsen2222@gmail.com>
2023-06-20 20:10:29 +0300

    update payment status request

--- store_owner/lib/module_orders/request/payment/paymnet_status_request.dart
+++ store_owner/lib/module_orders/request/payment/paymnet_status_request.dart
@@ -10,7 +10,9 @@ class PaymentStatusRequest {
   int? paymentFor;
 
   /// PAYMENT_GETAWAY_IN_APP_PURCHASE_APPLE_CONST = 225
+  /// 
   /// PAYMENT_GETAWAY_IN_APP_PURCHASE_GOOGLE_CONST = 226
+  /// 
   /// PAYMENT_GETAWAY_TAP_PAYMENT_CONST = 227
   int? paymentGetaway;
 
@@ -22,6 +24,19 @@ class PaymentStatusRequest {
   /// optional
   int? clientAddress;
 
+  /// for this app it will by 229 or 231
+  /// 
+  /// REAL_PAYMENT_BY_STORE_CONST = 229
+  /// 
+  /// REAL_PAYMENT_BY_ADMIN_CONST = 230
+  /// 
+  /// MOCK_PAYMENT_BY_STORE_CONST = 231
+  /// 
+  /// MOCK_PAYMENT_BY_ADMIN_CONST = 232
+  /// 
+  /// MOCK_PAYMENT_BY_SUPER_ADMIN_CONST = 233  
+  int? paymentType;
+
   PaymentStatusRequest({
     required this.status,
     this.paymentFor,
@@ -29,6 +44,7 @@ class PaymentStatusRequest {
     this.amount,
     this.paymentId,
     this.clientAddress,
+    this.paymentType = 229,
   });
 
   Map<String, dynamic> toMap() {
@@ -46,10 +62,12 @@ class PaymentStatusRequest {
     return PaymentStatusRequest(
       status: (map['status'] ?? 0) as int,
       paymentFor: map['paymentFor'] != null ? map['paymentFor'] as int : null,
-      paymentGetaway: map['paymentGetaway'] != null ? map['paymentGetaway'] as int : null,
+      paymentGetaway:
+          map['paymentGetaway'] != null ? map['paymentGetaway'] as int : null,
       amount: map['amount'] != null ? map['amount'] as int : null,
       paymentId: map['paymentId'] != null ? map['paymentId'] as int : null,
-      clientAddress: map['clientAddress'] != null ? map['clientAddress'] as int : null,
+      clientAddress:
+          map['clientAddress'] != null ? map['clientAddress'] as int : null,
     );
   }
 
