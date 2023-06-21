df90164448a7b40025f4bb14af3e03121f4470f5
ralsalloum <rami.alsalloum@hotmail.com>
2023-06-21 19:38:40 +0300

    epaymentbystore API had been updated

--- backend/src/Request/EPayment/EPaymentCreateByStoreOwnerRequest.php
+++ backend/src/Request/EPayment/EPaymentCreateByStoreOwnerRequest.php
@@ -9,6 +9,7 @@ class EPaymentCreateByStoreOwnerRequest
 {
     private int $status;
 
+    // could be store owner user id, store owner profile id, or store owner profile entity
     private int|StoreOwnerProfileEntity $storeOwnerProfile;
 
     private ?int $paymentFor;
@@ -17,13 +18,13 @@ class EPaymentCreateByStoreOwnerRequest
 
     private ?float $amount;
 
-    private ?string $clientAddress = null;
-
     private ?string $paymentId;
 
     private int|null|SubscriptionEntity $subscription = null;
 
     private int $paymentType;
+
+    private int $createdBy;
 
     public function getStatus(): int
     {
@@ -58,5 +59,25 @@ class EPaymentCreateByStoreOwnerRequest
     public function setPaymentType(int $paymentType): void
     {
         $this->paymentType = $paymentType;
+    }
+
+    public function setPaymentFor(?int $paymentFor): void
+    {
+        $this->paymentFor = $paymentFor;
+    }
+
+    public function setPaymentGetaway(?int $paymentGetaway): void
+    {
+        $this->paymentGetaway = $paymentGetaway;
+    }
+
+    public function setAmount(?float $amount): void
+    {
+        $this->amount = $amount;
+    }
+
+    public function setCreatedBy(int $createdBy): void
+    {
+        $this->createdBy = $createdBy;
     }
 }
