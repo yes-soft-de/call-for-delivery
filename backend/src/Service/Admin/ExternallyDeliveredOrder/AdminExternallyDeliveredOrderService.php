646110d8b3f5d2433115943e7cf2f5ff3c9010fa
ralsalloum <rami.alsalloum@hotmail.com>
2023-06-26 00:36:20 +0300

    createExternallyDeliveredOrderByAdmin function had been updated

--- backend/src/Service/Admin/ExternallyDeliveredOrder/AdminExternallyDeliveredOrderService.php
+++ backend/src/Service/Admin/ExternallyDeliveredOrder/AdminExternallyDeliveredOrderService.php
@@ -5,12 +5,14 @@ namespace App\Service\Admin\ExternallyDeliveredOrder;
 use App\AutoMapping;
 use App\Constant\AppFeature\AppFeatureResultConstant;
 use App\Constant\ExternalDeliveryCompany\ExternalDeliveryCompanyResultConstant;
+use App\Constant\ExternalDeliveryCompany\Mrsool\MrsoolCompanyConstant;
 use App\Constant\HTTP\HttpResponseConstant;
 use App\Constant\Order\OrderResultConstant;
 use App\Constant\OrderLog\OrderLogActionTypeConstant;
 use App\Constant\OrderLog\OrderLogCreatedByUserTypeConstant;
 use App\Entity\ExternallyDeliveredOrderEntity;
 use App\Entity\OrderEntity;
+use App\Manager\Admin\ExternallyDeliveredOrder\AdminExternallyDeliveredOrderManager;
 use App\Request\Admin\ExternallyDeliveredOrder\ExternallyDeliveredOrderCreateByAdminRequest;
 use App\Response\Admin\ExternallyDeliveredOrder\ExternallyDeliveredOrderCreateByAdminResponse;
 use App\Service\ExternallyDeliveredOrderHandle\ExternallyDeliveredOrderHandleService;
@@ -21,13 +23,33 @@ class AdminExternallyDeliveredOrderService
     public function __construct(
         private AutoMapping $autoMapping,
         private ExternallyDeliveredOrderHandleService $externallyDeliveredOrderHandleService,
-        private OrderLogService $orderLogService
+        private OrderLogService $orderLogService,
+        private AdminExternallyDeliveredOrderManager $adminExternallyDeliveredOrderManager
     )
     {
     }
 
+    public function getExternallyDeliveredOrdersByOrderId(int $orderId): array
+    {
+        return $this->adminExternallyDeliveredOrderManager->getExternallyDeliveredOrdersByOrderId($orderId);
+    }
+
+    public function updateExternallyDeliveredOrderStatusById(int $id, string $status): ?ExternallyDeliveredOrderEntity
+    {
+        return $this->adminExternallyDeliveredOrderManager->updateExternallyDeliveredOrderStatusById($id, $status);
+    }
+
     public function createExternallyDeliveredOrderByAdmin(ExternallyDeliveredOrderCreateByAdminRequest $request, int $adminUserId): int|string|ExternallyDeliveredOrderCreateByAdminResponse
     {
+        $externallyDeliveredOrders = $this->getExternallyDeliveredOrdersByOrderId($request->getOrderId());
+
+        if (count($externallyDeliveredOrders) > 0) {
+            foreach ($externallyDeliveredOrders as $externallyDeliveredOrder) {
+                $this->updateExternallyDeliveredOrderStatusById($externallyDeliveredOrder->getId(),
+                    MrsoolCompanyConstant::CANCELED_ORDER_STATUS_CONST);
+            }
+        }
+
         $externallyDeliveredOrder = $this->externallyDeliveredOrderHandleService->createExternallyDeliveredOrderByAdmin($request);
 
         if (($externallyDeliveredOrder === AppFeatureResultConstant::APP_FEATURE_NOT_ACTIVATED_CONST)
