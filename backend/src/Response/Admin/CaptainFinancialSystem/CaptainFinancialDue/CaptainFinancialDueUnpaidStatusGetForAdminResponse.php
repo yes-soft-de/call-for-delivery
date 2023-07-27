924309ef90f77f9f060d8d4bf441a2cd05d51a19
ralsalloum <rami.alsalloum@hotmail.com>
2023-07-27 21:44:18 +0300

    unpaidcaptainfinancialduebyadmin API added

---
+++ backend/src/Response/Admin/CaptainFinancialSystem/CaptainFinancialDue/CaptainFinancialDueUnpaidStatusGetForAdminResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Response\Admin\CaptainFinancialSystem\CaptainFinancialDue;
+
+use DateTime;
+
+class CaptainFinancialDueUnpaidStatusGetForAdminResponse
+{
+    public int $id;
+
+    public float $amount;
+
+    public DateTime $startDate;
+
+    public float $amountForStore;
+
+    public float $finalAmount;
+
+    //active = 1, inactive = 0
+    public int $state;
+
+    public int $lastPaymentId;
+
+    public float $lastPaymentAmount;
+
+    public ?DateTime $lastPaymentDate;
+}
