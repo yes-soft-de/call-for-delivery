33b02f52e00e94bd6cdd5c27aa30e7688ba2212e
ralsalloum <rami.alsalloum@hotmail.com>
2023-08-10 18:31:33 +0300

    updateStreetLineOrderStatus function updated

--- backend/src/Service/StreetLine/StreetLineOrderService.php
+++ backend/src/Service/StreetLine/StreetLineOrderService.php
@@ -3,22 +3,28 @@
 namespace App\Service\StreetLine;
 
 use App\AutoMapping;
+use App\Constant\ExternalDeliveryCompany\StreetLine\StreetLineCompanyConstant;
 use App\Constant\ExternallyDeliveredOrder\ExternallyDeliveredOrderConstant;
 use App\Constant\OrderLog\OrderLogActionTypeConstant;
 use App\Constant\OrderLog\OrderLogCreatedByUserTypeConstant;
+use App\Constant\Subscription\SubscriptionConstant;
 use App\Entity\ExternallyDeliveredOrderEntity;
 use App\Entity\OrderEntity;
+use App\Entity\SubscriptionEntity;
 use App\Request\StreetLine\StreetLineOrderStatusUpdateRequest;
 use App\Response\StreetLine\StreetLineOrderStatusUpdateResponse;
 use App\Service\ExternalOrderUpdateHandler\ExternalOrderUpdateHandlerService;
 use App\Service\OrderLog\OrderLogService;
+use App\Service\Subscription\SubscriptionService;
+use DateTimeInterface;
 
 class StreetLineOrderService
 {
     public function __construct(
         private AutoMapping $autoMapping,
         private ExternalOrderUpdateHandlerService $externalOrderUpdateHandlerService,
-        private OrderLogService $orderLogService
+        private OrderLogService $orderLogService,
+        private SubscriptionService $subscriptionService
     )
     {
     }
@@ -33,7 +39,12 @@ class StreetLineOrderService
         if ($order === ExternallyDeliveredOrderConstant::EXTERNALLY_DELIVERED_ORDER_NOT_EXIST_CONST) {
             return ExternallyDeliveredOrderConstant::EXTERNALLY_DELIVERED_ORDER_NOT_EXIST_CONST;
         }
-
+        // If order had been delivered, then update subscription cost of the store's subscription
+        if ($request->getStatus() === StreetLineCompanyConstant::ORDER_DELIVERED_STATUS_CONST) {
+            $this->handleUpdatingStoreSubscriptionCost($order->getOrderId()->getStoreOwner()->getId(),
+                $order->getOrderId()->getCreatedAt(), SubscriptionConstant::OPERATION_TYPE_ADDITION,
+                $order->getOrderId()->getDeliveryCost());
+        }
         // save order log
         $this->createOrderLogMessageViaOrderEntityAndByStreetLineCompany($order->getOrderId(), [
             'externalCompanyName' => $order->getExternalDeliveryCompany()->getCompanyName()
@@ -49,5 +60,19 @@ class StreetLineOrderService
         $this->orderLogService->createOrderLogMessage($orderEntity, 2, OrderLogCreatedByUserTypeConstant::STREETLINE_EXTERNAL_DELIVERY_COMPANY_CONST,
             OrderLogActionTypeConstant::ORDER_STATUS_UPDATE_BY_STREETLINE_EXTERNAL_COMPANY_CONST, $details,
             null, null);
+    }
+
+    /**
+     * Handles the updating of the subscriptionCost field of last store subscription
+     */
+    private function handleUpdatingStoreSubscriptionCost(
+        int $storeOwnerProfileId,
+        DateTimeInterface $orderCreatedAt,
+        string $operationType,
+        ?float $orderDeliveryCost = null
+    ): SubscriptionEntity|int|string
+    {
+        return $this->subscriptionService->handleUpdatingStoreSubscriptionCost($storeOwnerProfileId, $orderCreatedAt,
+            $operationType, $orderDeliveryCost);
     }
 }
